ac7874bddafc11039eb0f5624d482eee5459e14a
Karen Roberts <karenroberts@Karens-MacBook-Pro.local>
2018-11-13 14:52:59 +0000

    Challenge 14 Sinatra: POSTed params - we learned about POSTed parameters

--- app.rb
+++ app.rb
@@ -6,9 +6,13 @@ get '/cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   @last_name = params[:last_name]
   erb :index
+end
+
+get '/cat-form' do
+  erb :cat_form
 end
